99e5d5fe174da7c6174bb17f40ae820baa4e585d
ankit6370232757 <ankitkumarrath5@gmail.com>
2025-06-18 09:04:52 +0530

    all file are upload

---
+++ june/week1/day6/learnjs.js
@@ -0,0 +1,59 @@
+console.log("This is my external page")
+let a = 10;
+console.log(a);
+let A = 20;
+console.log(A);
+
+let name = "Kaibalya";
+console.log(name);
+
+let lastname = "Sahoo";
+console.log(lastname);
+
+let _Id = 123;
+console.log(_Id);
+
+let $rollno = 21;
+console.log($rollno);
+
+// how to declare a variable
+// ans var, let,const
+
+var b = 30;
+console.log(b); {
+    console.log(b);
+
+}
+
+function printValue() {
+    console.log(b);
+
+}
+printValue()
+    // declare in block level using var keyword
+    {
+        var c = 40;
+        console.log(c);
+
+    }
+console.log(c);
+
+function AB(params) {
+    console.log(c);
+
+}
+// function declare
+
+let d = 143;
+console.log(d); {
+    console.log(d);
+
+}
+
+function Prom(params) {
+    console.log(d);
+
+}
+Prom()
+const moive = "salar";
+console.log(moive);
