2433ed5177932a0277c703a2f2f50bd6baa13beb
ankit6370232757 <ankitkumarrath5@gmail.com>
2025-06-24 11:01:28 +0530

    React learng

---
+++ june/week3/day2/firstReact/LearnReact/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Home from './components/Home'
+
+function App() {
+
+
+
+  return (
+    
+    <div id='contend'>
+      <h1>Welcome to React World</h1>
+      <h1>Qilth Innovation</h1>
+      <Home/>
+    </div>
+    
+  )
+}
+
+export default App
---
+++ june/week3/day2/firstReact/LearnReact/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+
+      <a href="#"> This is my home page</a>
+    </div>
+  )
+}
+
+export default Home
+
